03018d741b04d425a603c1357aa12fa251b8b854
lfvperes <luisfvperes@gmail.com>
2025-07-09 21:51:26 -0300

    getting a random quote

--- index.ts
+++ index.ts
@@ -2,31 +2,8 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 async function main() {
-    let allIDs: number[] = [];
-    for (let i = 1; i <= 5; i++) {
-        let data: string | undefined;
-        try {
-            data = fs.readFileSync(path.join(__dirname, `./subtitles/${i}.srt`), 'utf8');
-        } catch (err) {
-            console.error('Error reading file:', err);
-            continue;
-        }
-        
-        // get subtitle numbers
-        if (data != undefined) {            
-            let subtIDNoMusicStr: RegExpMatchArray | null;
-            subtIDNoMusicStr = data.match(/[0-9]+(?=\n.+-->.+\n[^♪].+(\n.+)*)/g);
-            
-            if (subtIDNoMusicStr == null) {
-                console.log(`no matches found for ${i}.srt`);
-                continue;
-            }
-            
-            const subtIDNoMusicInt = subtIDNoMusicStr?.map( idStr => parseInt(idStr) + i * 10000)
-            
-            allIDs.push(...subtIDNoMusicInt)
-        }
-    }
+    const allIDsStr = fs.readFileSync(path.join(__dirname, `allIDs.txt`), 'utf8');
+    const allIDs: number[] = allIDsStr.split('\n').filter(Boolean).map(Number);
     
     const chooseID = Math.floor(Math.random() * allIDs.length + 1);
     console.log(`ID stored in ${chooseID}: ${allIDs[chooseID]}`)
@@ -34,10 +11,11 @@ async function main() {
     const fileNumber = Math.floor(allIDs[chooseID] / 10000);
     const quoteIdx = allIDs[chooseID] % 10000;
     console.log(`ID: ${allIDs[chooseID]}, file ${fileNumber}, quote number ${quoteIdx}`);
+
     const data = fs.readFileSync(path.join(__dirname, `./subtitles/${fileNumber}.srt`), 'utf8');
-    const regEx = new RegExp(`${quoteIdx}(\n.+-->.+\n[^♪].+(\n.+)*)`, 'g');
+    const regEx = new RegExp(`(?<=${quoteIdx}+\n.+-->.+\n)(.+\n)+`, 'g');
     const pulledQuote = data.match(regEx);
-    console.log(pulledQuote);
+    console.log(pulledQuote[0]);
 }
 
 main();
